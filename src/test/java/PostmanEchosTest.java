3b6047c3c26d9f7cf1fe64870ce1106ca66dc574
QA-Lexx <ilin_001@mail.ru>
2023-01-31 02:57:48 +0300

    create new files

---
+++ src/test/java/PostmanEchosTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PostmanEchosTest {
+
+    @Test
+    void shouldReturnPostmanEchosTest() {
+// Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("https://postman-echo.com/post") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(404)
+                .body("data", equalTo("some value"))
+        ;
+    }
+}
